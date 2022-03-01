f309913f7cdb422e2dcd700ae70803ca3fb5f327
Janetta Kataoeva <jkattoeva@gmail.com>
2022-03-01 21:02:42 +0600

    learned js function

---
+++ script.js
@@ -0,0 +1,29 @@
+// var message = prompt("What is your message?", "no message"); //prompt
+// if (message === "") {
+//   message  = "no message";
+// }
+// alert(message);
+
+//string "1991"
+//number 1991
+//boolean true false
+
+
+
+// var year = prompt("What year Kyrgyzstan gained independence?"); //prompt всегда возвращает строку!
+// if (year === "1991") {
+//   alert("Correct");
+// }
+// else {
+//   alert("incorrect")
+// }
+
+
+// var age = prompt("How old are you?");
+// var year = 2020 - age;
+// console.log(year);
+
+// alert();
+// console.log(); наводив мышкой на функции можем узнать как правильно использовать те или иные функции
+// confirm();
+
